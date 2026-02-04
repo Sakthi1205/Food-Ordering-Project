501724b550bbfa8bd0b002663a300af83059ec71
Sujindharsivakumar <sujisiva71@gmail.com>
2026-02-04 17:50:10 +0530

    Add files via upload

---
+++ adminuser.java
@@ -0,0 +1,67 @@
+package h1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class adminuser
+ */
+@WebServlet("/adminuser")
+public class adminuser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public adminuser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		int id=Integer.parseInt(request.getParameter("id"));//change according admin html page
+		String name=request.getParameter("name");//change according admin html page
+		String pass=request.getParameter("pass");//change according admin html page
+		PrintWriter o=response.getWriter();
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/exam","root","root@39");//change the portal and localhost
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("Select * from admin");
+			while(rs.next()) {
+				if(id!=rs.getInt("id") || name!=rs.getString("name") || pass!=rs.getString("pass")) {
+					o.println("Your credentials are invalid");
+					o.println("Please give correct data");
+					o.println("<a href='admin.html'>Back To Login</a>");//change it
+				}
+			}
+			response.sendRedirect("option.html");//change it
+		}
+		catch(Exception e) {
+			o.println("Error:"+e.getMessage());
+		}
+	}
+
+}
